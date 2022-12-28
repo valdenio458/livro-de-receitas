f1a2ec42065859810473399f1ecd0b61033cf155
valdenio458 <valdeniomartinho@gmail.com>
2022-12-28 16:19:23 -0300

    Iniciando projeto

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index 
-      r1 = Recipe.new(name:'Pudim', recipe_type:'Sobremesa')
-      r2 = Recipe.new(name:'Bolo de fubá', recipe_type:'Sobremesa')
+      r1 = Recipe.new(name:'Pudim')
+      r2 = Recipe.new(name:'Bolo de fubá')
 
       @recipes = [r1,r2]
   end
