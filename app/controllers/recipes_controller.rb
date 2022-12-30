78d7c69782b1b747e923e036a047d95fb6a5d2d4
valdenio458 <valdeniomartinho@gmail.com>
2022-12-30 17:31:54 -0300

    Adiciona formulário para criação de novas receitas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,23 @@ class RecipesController < ApplicationController
   def show 
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe = Recipe.new(
+      name: params[:recipe][:name],
+      ingredients: params[:recipe][:ingredients],
+      cook_method: params[:recipe][:cook_method],
+      cook_time: params[:recipe][:cook_time],
+    )
+    if recipe.save 
+      return redirect_to recipe_path(recipe.id)
+    end
+    
+    render :new
+  end 
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root  to: 'home#index'
-  get 'receitas/:id', to: 'recipes#show', as: 'recipe'
+
+  resources :recipes, only:[:show, :new, :create]
 end
