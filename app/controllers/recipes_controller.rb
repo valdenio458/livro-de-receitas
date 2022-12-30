b5acd7494e6535ee44e31eca99f30dec789afa9e
valdenio458 <valdeniomartinho@gmail.com>
2022-12-30 17:37:36 -0300

    Adiciona link de home

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,14 +8,14 @@ class RecipesController < ApplicationController
   end
 
   def create
-    recipe = Recipe.new(
+    @recipe = Recipe.new(
       name: params[:recipe][:name],
       ingredients: params[:recipe][:ingredients],
       cook_method: params[:recipe][:cook_method],
       cook_time: params[:recipe][:cook_time],
     )
-    if recipe.save 
-      return redirect_to recipe_path(recipe.id)
+    if @recipe.save 
+      return redirect_to recipe_path(@recipe.id)
     end
     
     render :new
